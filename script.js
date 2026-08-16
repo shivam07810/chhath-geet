c047b175436eae3f3830dc0df38e7b46ad60314c
shivam07810 <shivam07810@gmail.com>
2026-08-16 09:02:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,21 @@
-const audio = document.getElementById("audio");
-const playBtn = document.getElementById("playBtn");
-const progress = document.getElementById("progress");
-const current = document.getElementById("current");
-const duration = document.getElementById("duration");
-const time = document.getElementById("time");
-
-let playing = false;
-
-// Live Clock
-function updateClock() {
-  const now = new Date();
-  let h = now.getHours();
-  let m = now.getMinutes();
-
-  const ampm = h >= 12 ? "PM" : "AM";
-  h = h % 12;
-  if (h === 0) h = 12;
-
-  h = String(h).padStart(2, "0");
-  m = String(m).padStart(2, "0");
-
-  time.innerHTML = h + ":" + m + " " + ampm;
-}
-
-setInterval(updateClock, 1000);
-updateClock();
-
-// Play / Pause
-function playPause() {
-  if (playing) {
-    audio.pause();
-    playBtn.innerHTML = "▶";
-  } else {
-    audio.play();
-    playBtn.innerHTML = "⏸";
-  }
-  playing = !playing;
-}
-
-//
 audio.addEventListener("loadedmetadata", () => {
-  progress.max = Math.floor(audio.duration);
-  duration.textContent = format(audio.duration);
+    progress.max = Math.floor(audio.duration);
+    duration.textContent = format(audio.duration);
 });
 
 audio.addEventListener("timeupdate", () => {
-  progress.value = Math.floor(audio.currentTime);
-  current.textContent = format(audio.currentTime);
+    progress.value = Math.floor(audio.currentTime);
+    current.textContent = format(audio.currentTime);
 });
 
 progress.addEventListener("input", function () {
-  audio.currentTime = this.value;
+    audio.currentTime = this.value;
 });
+
+function format(sec) {
+    if (isNaN(sec)) return "0:00";
+    let min = Math.floor(sec / 60);
+    let s = Math.floor(sec % 60);
+    if (s < 10) s = "0" + s;
+    return min + ":" + s;
+}
